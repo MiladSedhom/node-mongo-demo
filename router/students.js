1d413a00d90dc30e8f4e9ba2c092e7e2c826950d
MiladSedhom <MiladSedhom@gmail.com>
2024-07-31 03:46:49 +0300

    refactor edit student

--- router/students.js
+++ router/students.js
@@ -70,16 +70,71 @@ router.post('/', async (req, res) => {
 	}
 })
 
-router.put('/:id', (req, res) => {
-	const { id } = req.params
+router.put('/:id', async (req, res) => {
+	try {
+		const { id } = req.params
+		let { firstName, lastName, skills, addresses } = req.body
 
-	Student.findByIdAndUpdate(id, req.body)
-		.then(r => {
-			res.send(r)
-		})
-		.catch(e => {
-			res.status(500).json({ message: e.message })
-		})
+		console.log(req.body)
+
+		const student = await Student.findById(id)
+		if (!student) return res.status(404).json({ message: 'Student not found' })
+
+		student.firstName = firstName
+		student.lastName = lastName
+
+		if (skills) {
+			await Skill.updateMany(
+				{ _id: { $in: student.skills, $nin: skills.map(s => s._id) } },
+				{ $pull: { students: student._id } }
+			)
+
+			await Skill.updateMany(
+				{ _id: { $in: skills.map(s => s._id), $nin: student.skills } },
+				{ $addToSet: { students: student._id } }
+			)
+
+			student.skills = skills.map(s => s._id)
+		}
+
+		if (addresses) {
+			await Address.deleteMany({
+				_id: {
+					$in: student.addresses,
+					$nin: addresses.map(a => {
+						if (a._id) return a._id
+					}),
+				},
+				student: student._id,
+			})
+
+			// Add or update new addresses
+			for (let address of addresses) {
+				if (address._id) {
+					console.log('if')
+					await Address.findByIdAndUpdate(address._id, {
+						...address,
+						student: student._id,
+					})
+				} else {
+					console.log('else')
+					console.log(address)
+					const newAddress = new Address({ ...address, _id: undefined, student: student._id })
+					await newAddress.save()
+					student.addresses.push(newAddress._id)
+				}
+			}
+		}
+
+		await student.save()
+
+		const updatedStudent = await Student.findById(id).populate('skills').populate('addresses')
+
+		res.status(200).json(updatedStudent)
+	} catch (e) {
+		console.error('Error updating student:', e)
+		res.status(500)
+	}
 })
 
 router.delete('/:id', (req, res) => {
